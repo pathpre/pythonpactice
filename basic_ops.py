9286e8fc13da1ae96f386ee923b3b7680b7d959a
Presha Pathak <pathak.presha24@gmail.com>
2024-03-05 06:13:40 +0000

    Accepting Values and operations

---
+++ basic_ops.py
@@ -0,0 +1,10 @@
+x = float(input ("Enter first number: "))
+y= float(input("Enter Second Number: "))
+#operations
+sum = x+y
+diff = x-y
+prod= x*y
+div= x/y
+mod= x%y
+print(sum, diff, prod , div,mod)
+
